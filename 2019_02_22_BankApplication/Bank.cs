bea2de7fbd2ddc8eb58ce9fc7b2df374da6f8207
bartab9610 <barnabas96@freemail.hu>
2019-02-22 13:36:39 +0100

    Keresés - Utalás - Egyenleg lekérés

--- 2019_02_22_BankApplication/Bank.cs
+++ 2019_02_22_BankApplication/Bank.cs
@@ -12,11 +12,44 @@ namespace _2019_02_22_BankApplication
         {
             public string Nev { get; set; }
             public string Szamlaszam { get; set; }
+            public ulong Osszeg { get; set; }
+
+            public Szamla(string nev, string szamlaszam)
+            {
+                this.Nev = nev;
+                this.Szamlaszam = szamlaszam;
+                this.Osszeg = 0;
+            }
         }
 
+        List<Szamla> szamlak = new List<Szamla>();
         public void Ujszamla(string nev, string szamlaszam)
         {
+            var sz = new Szamla(nev, szamlaszam); // Szamla sz = new Szamla(nev, szamlaszam);
+            szamlak.Add(sz);
+        }
 
+        Szamla Keres(string szamlaszam)
+        {
+            foreach (var sz  in szamlak)
+            {
+                if (sz.Szamlaszam.Equals(szamlaszam))
+                {
+                    return sz;
+                }
+            }
+            return null;
+        }
+        public void Utal(string honnan, string hova, ulong osszeg) // ulong -> előjel nélküli
+        {
+            var Forras_szamla = Keres(honnan);
+            var Cel_szamla = Keres(hova);
+            Forras_szamla.Osszeg -= osszeg;
+            Cel_szamla.Osszeg += osszeg;
+        }
+        public ulong Egyenleg(string szamlaszam)
+        {
+            return Keres(szamlaszam).Osszeg;
         }
     }
 }
--- 2019_02_22_BankApplication/Program.cs
+++ 2019_02_22_BankApplication/Program.cs
@@ -10,6 +10,14 @@ namespace _2019_02_22_BankApplication
     {
         static void Main(string[] args)
         {
+            Bank OTP = new Bank();
+            OTP.Ujszamla("Debreczeni.L", "1122334455");
+            OTP.Ujszamla("D.László", "9988776655");
+            OTP.Utal("1122334455", "9988776655", 10000);
+            Console.WriteLine(OTP.Egyenleg("1122334455"));
+            Console.WriteLine(OTP.Egyenleg("9988776655"));
+
+            Console.ReadLine();
         }
     }
 }
