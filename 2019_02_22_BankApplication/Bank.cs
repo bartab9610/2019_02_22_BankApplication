db571e0d5629531be1a8c826a004f40dd782c496
bartab9610 <barnabas96@freemail.hu>
2019-02-22 13:12:24 +0100

    Bank class

---
+++ 2019_02_22_BankApplication/Bank.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2019_02_22_BankApplication
+{
+    class Bank
+    {
+        class Szamla
+        {
+
+        }
+
+        public void Ujszamla(string nev, string szamlaszam)
+        {
+
+        }
+    }
+}
